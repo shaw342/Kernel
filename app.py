c88429470b8d8bfd6754c84ed26081bd849be970
VotreNom <shawanbarua70@gmail.com>
2023-10-15 23:29:35 +0200

    take json data

--- app.py
+++ app.py
@@ -20,23 +20,15 @@ async def logout(req):
     template = await jinja.render_async("logout.html",req)
     return response.html(template.body)
 
+
 @app.route("/register",methods=["POST"])
 async def register(req):
-    username = req.json.get("username")
-    email = req.json.get("email")
-    password = req.json.get("password")
     
-    data = {
-        username:username,
-        email:email,
-        password:password
-    }
-    try:
-        Model.user_data(data)
-    except Exception as e:
-        raise (e)
-    
-    return response.html("<h1>connexion reussie</h1>")
+    username = req.json().get("username")
+    email = req.json().get("email")
+    password = req.json().get("password")
+        # Traitez les données JSON ici  
+    return response.text("hello")
 
 if __name__ == "__main__":
     app.run(debug=True)
