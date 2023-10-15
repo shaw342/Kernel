eb6f931e8da319e84c286670f4b730a400bece5b
VotreNom <shawanbarua70@gmail.com>
2023-10-15 20:17:37 +0200

    json format

--- app.py
+++ app.py
@@ -20,11 +20,23 @@ async def logout(req):
     template = await jinja.render_async("logout.html",req)
     return response.html(template.body)
 
-@app.route("resgister",methods=["POST"])
+@app.route("/register",methods=["POST"])
 async def register(req):
-    data = req.json
-    Model.user_data(data)
-    return text("connexion reussie !")
+    username = req.json.get("username")
+    email = req.json.get("email")
+    password = req.json.get("password")
+    
+    data = {
+        username:username,
+        email:email,
+        password:password
+    }
+    try:
+        Model.user_data(data)
+    except Exception as e:
+        raise (e)
+    
+    return response.html("<h1>connexion reussie</h1>")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8000,debug=True)
+    app.run(debug=True)
--- templates/index.js
+++ templates/index.js
@@ -1,14 +1,27 @@
-fetch('http://localhost:8000/logout"', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ key: 'value' })
-})
+
+let username = document.getElementById("username")
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+
+let data = {
+    username:username,
+    email:email,
+    password:password,
+} 
+
+url = "http://localhost:8000/logout"
+fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    console.log(data)
+    
   })
   .catch(error => {
-    console.error('ERROR', error);
+    console.error(error)
   });
