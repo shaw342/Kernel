9c3ce0fd1b2dfeea9b2d0d1364654fb12e5cb4ca
VotreNom <shawanbarua70@gmail.com>
2023-11-04 00:41:53 +0100

    script

--- database/script.py
+++ database/script.py
@@ -9,25 +9,26 @@ class Model():
         self.client = fauna_client.Client() # domain="db.eu.fauna.com"    
         
     def user_data(self, data: dict):
-        query = fauna.fql("users.create(${data})", data=data)
-        self.client.query(query)
-
-    def product_data(self,data: dict):
-        pass
+        query = fauna.fql("let customers = Customers.create(${data})", data=data)
+        return self.client.query(query)
+        
         
     def verification(self, data: dict) -> bool:
-        query = fauna.fql("users.byMail(${mail}).nonEmpty()", mail=data['mail'])
+        query = fauna.fql("Customers.byMail(${mail}).nonEmpty()", mail=data['mail'])
         result = self.client.query(query)
         return not result.data
     
     def name_verification(self,data: dict)-> bool:
-        query = fauna.fql("users.byUser(${user}).nonEmpty()", user=data["user"])
+        query = fauna.fql("Customers.byUser(${user}).nonEmpty()", user=data["user"])
         result = self.client.query(query)
         return not result.data
     
     
     def confirmation_login(self,mail: str,user: str) -> bool:
-        query = fauna.fql("{uniqueUser: users.byUser(${user}).nonEmpty(),uniqueMail: users.byUser(${mail}).nonEmpty()}",user = user, mail = mail)
+        query = fauna.fql("{uniqueUser: Customers.byUser(${user}).nonEmpty(),uniqueMail: Customers.byUser(${mail}).nonEmpty()}",user = user, mail = mail)
         result = self.client.query(query)
         return not any(result.data.values())
     
+    def Order_data(self,data):
+        query = fauna.fql("Orders.create(${data})",data)
+        self.client.query(query)
