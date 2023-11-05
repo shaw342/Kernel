d1ff88efce4e0885e88a20caa7cfd0498e2683d2
VotreNom <shawanbarua70@gmail.com>
2023-11-05 20:37:18 +0100

    search

--- templates/search.js
+++ templates/search.js
@@ -1,77 +1,25 @@
-document.getElementById("search").addEventListener("click",(event) =>{
-    let searchInfo = document.getElementById("search-text").value;
-    let fail = document.getElementById("fail");
-    data =[
-        {
-            name:"acer",
-            price:400,
-            os:"ubuntu linux"
+let count = 0;
+document.getElementById("add_basket").addEventListener("click", (e) => {
+    count++;
+    let names = document.getElementsByClassName("name");
+    let oses = document.getElementsByClassName("os");
+    let prices = document.getElementsByClassName("price");
+
+    let data = {
+        name: names[0].textContent, 
+        os:  oses[0].textContent, 
+        price: prices[0].textContent, 
+        quantity:count,
+    }
+
+    fetch("/Orders",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
         },
-        {
-            name:"dell",
-            price:600,
-            os:"ubuntu debian"
-        }
-]
-
-let name =[]
-
-for (let i = 0; i<data.length;i++){
-    name[i] = data[i].name
-}
-
-function filtreTexte(arr, requete) {
-    return arr.filter(function (el) {
-      return el.toLowerCase().indexOf(requete.toLowerCase()) !== -1;
-    });
-  }
-  if (filtreTexte(name,searchInfo).length === 0){
-    fail.innerHTML = "FAIL"
-  }else{
-    console.log(filtreTexte(name,searchInfo));
-
-  }
-  
-}) 
-
-document.getElementById("search-text").addEventListener("keyup",(e)=>{
-    const letter = e.target.value;
-    const box = document.getElementsByClassName("box")
-
-console.log(e.target.value);
-    data =[
-        {
-            name:"acer",
-            price:400,
-            os:"ubuntu linux"
-        },
-        {
-            name:"dell",
-            price:600,
-            os:"linux debian"
-        },
-        {
-            name:"hp",
-            price:300,
-            os:"arch linux"
-        }
-]
-let reelName = []
-for(i = 0;i<data.length;i++){
-    reelName[i] =data[i].name; 
-}
-
-function filter(arr, request){
-    return arr.filter(function (el){
-        return el.toLowerCase().indexOf(request.toLowerCase()) !== -1;
+        body:JSON.stringify(data),
+    }).then(async (res)=>{
+        let value = res.json()
+        console.log(value);
     })
-}
-if (filter(reelName,letter).length === 0){
-    console.error("hello");
-}else{
-    console.log(filter(reelName,letter));
-    const name = filter(reelName,letter);
-    const eventName = document.getElementById(name);
-    eventName.classList.add("newClass")
-}
 })
